fb65607f35229c41217d1244f2ce173fc73a058b
sajibsaifullah <dusajibphy@gmail.com>
2023-11-10 21:31:55 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// for bootstrap alert
+const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement("div");
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    "</div>",
+  ].join("");
+
+  alertPlaceholder.append(wrapper);
+};
+
+const alertTrigger = document.getElementById("liveAlertBtn");
+if (alertTrigger) {
+  alertTrigger.addEventListener("click", () => {
+    appendAlert("Nice, you triggered this alert message!", "success");
+  });
+}
+
+// for Collapse
+const collapseElementList = document.querySelectorAll(".collapse");
+const collapseList = [...collapseElementList].map(
+  (collapseEl) => new bootstrap.Collapse(collapseEl)
+);
+
+// for tooltip
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
